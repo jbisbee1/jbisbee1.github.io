f48781d3e69cc90493ad5728880f4ae26d291f21
James Bisbee <jimbisbee@yahoo.com>
2024-05-27 10:03:02 -0500

    more tweaks for mobile

--- assets/main.js
+++ assets/main.js
@@ -1,54 +1,50 @@
 $(document).ready(function() {
 
-    $(".popup-btn").click(function() {
-        $(".overlay, .popup").addClass("reveal");
-    });
+$(".popup-btn").click(function() {
+$(".overlay, .popup").addClass("reveal");
+});
 
-    $(".close-x").click(function() {
-        $(".overlay, .popup").removeClass("reveal");
-    });
+$(".close-x").click(function() {
+$(".overlay, .popup").removeClass("reveal");
+});
 
+$(".category-selectors .btn").click(function() {
+$(".category-selectors .btn").removeClass("active");
+$(this).addClass("active");
+});
 
-    $(".category-selectors .btn").click(function() {
-        $(".category-selectors .btn").removeClass("active");
-        $(this).addClass("active");
-    });    
+$(".grid-button").click(function() {
+$(".grid-button").removeClass("active");
+$(".overlay").addClass("reveal");
+$(".navigation-arrows").addClass("show");
+$("body").addClass("body-freeze");
+});
 
-    
+$(".overlay, .close-x").click(function() {
+$(this).removeClass("reveal");
+$(".grid-popup").removeClass("active");
+$(".navigation-arrows").removeClass("show");
+$("body").removeClass("body-freeze");
+});
 
-    $(".grid-button").click(function() {
-        $(".grid-button").removeClass("active");
-        $(".overlay").addClass("reveal");
-        $(".navigation-arrows").addClass("show");
-        $("body").addClass("body-freeze");
-    });
-    $(".overlay, .close-x").click(function() {
-        $(this).removeClass("reveal");
-        $(".grid-popup").removeClass("active");
-        $(".navigation-arrows").removeClass("show");
-        $("body").removeClass("body-freeze");
-    });
+$(".card-wrap-grid .grid-row:nth-child(n+4) .grid-button img").attr("loading", "lazy");
 
-    $(".card-wrap-grid .grid-row:nth-child(n+4) .grid-button img").attr("loading", "lazy");
-    
+$(".row-category p").click(function() {
+$(this).parent(".row-category").parent(".grid-row").toggleClass("show");
+});
 
-    $("#btn-published").click(function() {
-        $(".published-paper").addClass("emphasize");
-        $(".working-paper").removeClass("emphasize");
-    });    
+$("#btn-published").click(function() {
+$(".published-paper").addClass("emphasize");
+$(".working-paper").removeClass("emphasize");
+});
 
-    $("#btn-working").click(function() {
-        $(".working-paper").addClass("emphasize");
-        $(".published-paper").removeClass("emphasize");
-    });
+$("#btn-working").click(function() {
+$(".working-paper").addClass("emphasize");
+$(".published-paper").removeClass("emphasize");
+});
 
-    $(".grid-row").click(function() {
-        $(".grid-button").removeClass("emphasize");
-        $(".category-selectors .btn").removeClass("active");
-    });
-
-    
-    
-
-
+$(".grid-row").click(function() {
+$(".grid-button").removeClass("emphasize");
+$(".category-selectors .btn").removeClass("active");
+});
 })
