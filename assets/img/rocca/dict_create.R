b7767c03d54219de9d4d605078a6fdabc6728207
James Bisbee <jimbisbee@yahoo.com>
2024-10-04 19:11:33 -0400

    small tweaks

--- assets/img/rocca/dict_create.R
+++ assets/img/rocca/dict_create.R
@@ -1,3 +1,19 @@
 require(tidyverse)
 
-list.files()
+imgs <- list.files(pattern = 'jpg')
+
+dict <- NULL
+counter <- 1
+for(f in imgs) {
+  dict <- dict %>%
+    bind_rows(data.frame(filename = f,
+                         index = counter))
+  counter <- counter + 1
+}
+
+write_csv(dict,file = './dictionary_lookup.csv')
+
+for(i in 1:nrow(dict)) {
+  # dict[i,]
+  file.rename(dict$filename[i],paste0('image_',dict$index[i],'.jpg'))
+}
